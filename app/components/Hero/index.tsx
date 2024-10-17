9f1ce222356c39f0cee3e08d37db2f2d62ae7e05
ZingerLittleBee <6970999@gmail.com>
2024-10-17 19:21:24 +0800

    hero

--- app/components/Hero/index.tsx
+++ app/components/Hero/index.tsx
@@ -1,24 +1,16 @@
-import React from 'react'
+import Image from 'next/image'
 
-import { Page } from '@/payload/payload-types'
-import { HighImpactHero } from '../../_heros/HighImpact'
-import { LowImpactHero } from '../../_heros/LowImpact'
-import { MediumImpactHero } from '../../_heros/MediumImpact'
-
-const heroes = {
-  highImpact: HighImpactHero,
-  mediumImpact: MediumImpactHero,
-  lowImpact: LowImpactHero,
-}
-
-export const Hero: React.FC<Page['hero']> = props => {
-  const { type } = props || {}
-
-  if (!type || type === 'none') return null
-
-  const HeroToRender = heroes[type]
-
-  if (!HeroToRender) return null
-
-  return <HeroToRender {...props} />
+export default function Hero() {
+  return (
+    <div className="flex w-screen flex-col items-center justify-center">
+      <h1 className="mt-[59px] text-center text-[68px] font-normal leading-[77.79px] text-[#141414]">
+        <p>Smart Asset Management</p>
+        <p>for the AI Content Era</p>
+      </h1>
+      <button className="mb-[58px] mt-9 rounded-[1000px] bg-black px-[57.5px] py-[19px] text-[16px] font-normal leading-[20px] text-white">
+        Start for free
+      </button>
+      <Image src="/hero.png" width={1280} height={663} alt="muse hero" />
+    </div>
+  )
 }
--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,10 @@
 import SwiperBlock from '@/components/SwiperBlock'
+import Hero from '@/components/Hero'
 
 export default function LandingPage() {
   return (
-    <div>
+    <div className="grid">
+      <Hero />
       <SwiperBlock />
     </div>
   )
