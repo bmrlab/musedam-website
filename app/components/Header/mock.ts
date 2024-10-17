f279d52c8b8b1a5d7c82b9ee74a1573e7343394f
zhangye <zhangye@tezign.com>
2024-10-17 15:50:40 +0800

    nav ui

---
+++ app/components/Header/mock.ts
@@ -0,0 +1,49 @@
+export const data = [
+  {
+    title: 'Features',
+    href: '/features',
+    item: [
+      {
+        title: 'AI-Powered',
+        items: [
+          {
+            label: 'AI Search',
+            href: '',
+            icon: 'search',
+            description: 'Visual Content Asset Search',
+          },
+          {
+            label: 'AI Parsing',
+            href: '',
+            icon: 'search',
+            description: 'Visuals, Color Schemes, Themes, etc.',
+          },
+          {
+            label: 'AI Content Creation',
+            href: '',
+            icon: 'search',
+            description: 'Craft Blog from Asset Insights',
+          },
+          {
+            label: 'Auto Tags',
+            href: '',
+            icon: 'search',
+            description: 'Auto-Tag for Search & Clustering',
+          },
+          {
+            label: 'AI Search',
+            href: '',
+            icon: 'search',
+            description: 'Visual Content Asset Search',
+          },
+          {
+            label: 'AI Search',
+            href: '',
+            icon: 'search',
+            description: 'Visual Content Asset Search',
+          },
+        ],
+      },
+    ],
+  },
+]
