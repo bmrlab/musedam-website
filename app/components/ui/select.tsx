70eaa6df6d78174c117d81f33b9ecd6533e0b33a
ZingerLittleBee <6970999@gmail.com>
2024-10-21 16:25:10 +0800

    translation switcher

--- app/components/ui/select.tsx
+++ app/components/ui/select.tsx
@@ -10,6 +10,7 @@ import {
 import * as SelectPrimitive from "@radix-ui/react-select"
 
 import { cn } from "@/lib/utils"
+import Icons from "@/components/icon";
 
 const Select = SelectPrimitive.Root
 
@@ -31,7 +32,7 @@ const SelectTrigger = React.forwardRef<
   >
     {children}
     <SelectPrimitive.Icon asChild>
-      <CaretSortIcon className="h-4 w-4 opacity-50" />
+      <Icons.caretDown className="h-4 w-4" />
     </SelectPrimitive.Icon>
   </SelectPrimitive.Trigger>
 ))
