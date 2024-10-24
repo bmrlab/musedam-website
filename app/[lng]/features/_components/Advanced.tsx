311b37eabf1c5622574f575155e98026a2faed99
zhangye <zhangye@tezign.com>
2024-10-24 17:58:40 +0800

    features layout

---
+++ app/[lng]/features/_components/Advanced.tsx
@@ -0,0 +1,43 @@
+export default function AdvancedBlock() {
+  const features = [
+    {
+      title: 'Tags',
+      description: 'Tailored tags streamline workflows',
+    },
+    {
+      title: 'Metadata',
+      description: 'Comprehensive metadata for clarity',
+    },
+    {
+      title: 'Versions',
+      description: 'Organized versioning for easy updates',
+    },
+    {
+      title: 'Filters',
+      description: 'Advanced filters for precise asset retrieval',
+    },
+  ]
+
+  return (
+    <div className="w-full py-[80px]">
+      <h2 className="mb-[80px] text-center text-4xl">More than Just the Basics</h2>
+      <div className="border-b border-l border-t border-[#141414]">
+        <div className="grid h-[320px] grid-cols-1 items-center md:grid-cols-2 lg:grid-cols-4">
+          {features.map((feature, index) => (
+            <div
+              key={index}
+              className="flex h-full w-full flex-col items-start justify-center border-r border-[#141414] p-6 px-10"
+            >
+              <h3 className="mb-6 text-[24px] font-normal leading-6 text-[#141414]">
+                {feature.title}
+              </h3>
+              <p className="text-[16px] font-normal leading-[22px] text-[#3E3E59]">
+                {feature.description}
+              </p>
+            </div>
+          ))}
+        </div>
+      </div>
+    </div>
+  )
+}
---
+++ app/[lng]/features/ai-powered/page.tsx
@@ -0,0 +1,3 @@
+export default function Page() {
+  return <div className="flex w-full flex-col items-center py-[80px]">AiPowered</div>
+}
