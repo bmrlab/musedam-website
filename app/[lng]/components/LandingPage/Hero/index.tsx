2aa746472eadc25ee254945169d40cf186943096
ZingerLittleBee <6970999@gmail.com>
2024-10-28 15:09:15 +0800

    ui style

--- app/[lng]/components/LandingPage/Hero/index.tsx
+++ app/[lng]/components/LandingPage/Hero/index.tsx
@@ -9,31 +9,38 @@ import { useHover } from '@uidotdev/usehooks'
 export default function Hero() {
   return (
     <div className="flex w-full flex-col items-center justify-center px-[80px]">
-      <h1 className="mt-[59px] text-center text-[68px] font-normal leading-[77.79px] text-[#141414]">
+      <h1 className="mt-[59px] text-center font-baskervville text-[68px] font-normal leading-[77.79px] text-[#141414]">
         <p>Smart Asset Management</p>
         <p>for the AI Content Era</p>
       </h1>
-      <button className="mb-[58px] mt-9 rounded-[1000px] bg-black px-[57.5px] py-[19px] text-[16px] font-normal leading-[20px] text-white">
+      <button className="mb-[58px] mt-9 rounded-[1000px] bg-black px-[57.5px] py-[19px] font-mono text-[16px] font-normal leading-[20px] text-white">
         Start for free
       </button>
       <Banner />
       <div className="mt-[80px] flex w-full items-center justify-between">
-        <h1 className="text-[68px] font-normal leading-[77.79px] text-[#141414]">
+        <h1 className="font-baskervville text-[68px] font-normal leading-[77.79px] text-[#141414]">
           AI-Powered Asset Management
         </h1>
         <div className="flex max-w-[680px] flex-col items-start gap-10">
-          <p className="text-[16px] font-light leading-6 text-[#141414]">
+          <p className="font-mono text-[16px] font-light leading-6 text-[#141414]">
             Dive into the future of asset management with MuseDAM's cutting-edge AI system.
             Experience intelligent search, insightful smart parsing, and a robust suite of creative
             tools, all enhanced by our interactive MuseCopilot for a seamless experience.
           </p>
-          <button className="rounded-[8px] bg-black px-[42.5px] py-[14px] text-[16px] leading-5 text-white">
+          <button className="rounded-[8px] bg-black px-[42.5px] py-[14px] font-mono text-[16px] leading-5 text-white">
             Start for free
           </button>
         </div>
       </div>
-      <div className="relative mt-[80px] w-full px-10 h-[692px]">
-        <video width={1200} height={692} muted autoPlay loop className="absolute inset-0 h-full size-full object-cover">
+      <div className="relative mt-[80px] h-[692px] w-full px-10">
+        <video
+          width={1200}
+          height={692}
+          muted
+          autoPlay
+          loop
+          className="absolute inset-0 size-full h-full object-cover"
+        >
           <source src="/Introduction/Introduction.mp4" type="video/mp4" />
         </video>
       </div>
--- app/[lng]/components/Subscribe.tsx
+++ app/[lng]/components/Subscribe.tsx
@@ -11,7 +11,7 @@ export default function SubscribeBlock({ className }: { className?: string }) {
         className,
       )}
     >
-      <h2 className="text-[48px] font-normal leading-[54.91px] text-white">
+      <h2 className="font-baskervville text-[48px] font-normal leading-[54.91px] text-white">
         Subscribe for exclusive updates and resources
       </h2>
       <div className="flex gap-2.5">
@@ -20,11 +20,11 @@ export default function SubscribeBlock({ className }: { className?: string }) {
           placeholder="Your work email"
           className="h-[50px] w-[420px] rounded-[6px] border-none bg-white p-4"
         />
-        <button className="h-[50px] rounded-[6px] bg-[#043FFB] px-[56.5px] text-[16px] leading-[20.8px] text-white">
+        <button className="h-[50px] rounded-[6px] bg-[#043FFB] px-[56.5px] font-mono text-[16px] leading-[20.8px] text-white">
           Subscribe
         </button>
       </div>
-      <p className="text-[14px] leading-[28px]">
+      <p className="font-mono text-[14px] leading-[28px]">
         <span className="text-[#FAFAFA]">Kickstart your journey with our</span>
         <span className="text-[#0FF95C]"> free trial →</span>
       </p>
