bbc79558b728a4eb4f85965b135c0cb7f4fb14e1
zhangye <zhangye@tezign.com>
2024-10-29 15:32:49 +0800

    footer mobile ui

--- app/[lng]/components/Footer/index.tsx
+++ app/[lng]/components/Footer/index.tsx
@@ -1,25 +1,17 @@
 'use client'
 
-import React, { useCallback, useMemo, useState } from 'react'
+import React, { useMemo } from 'react'
 
 import Image from 'next/image'
 import { FOOTER_ITERM } from './mock'
-import { languages } from '@/i18n/settings'
-import {
-  Select,
-  SelectContent,
-  SelectItem,
-  SelectTrigger,
-  SelectValue,
-} from '@/components/ui/select'
-import { usePathname, useRouter } from 'next/navigation'
-import Icons from '@/components/icon'
-import SocialWidget from '@/[lng]/components/Footer/Social'
 
-export default function Footer(params: { lng: string }) {
-  const router = useRouter()
-  const pathname = usePathname()
-  const [lng, setLng] = useState(params.lng)
+import SocialWidget from '@/[lng]/components/Footer/Social'
+import useIsMobile from '@/hooks/useIsMobile'
+import FooterMobile from '@/[lng]/components/Footer/mobile'
+import LngSelector from '@/[lng]/components/Footer/LngSelector'
+
+export default function Footer({ lng }: { lng: string }) {
+  const isMobile = useIsMobile()
   const map = useMemo(() => {
     const map = new Map<
       string,
@@ -35,52 +27,13 @@ export default function Footer(params: { lng: string }) {
     return map
   }, [])
 
-  const onLngChange = useCallback(
-    (value: string) => {
-      router.replace(pathname.replace(lng, value), {
-        scroll: false,
-      })
-      setLng(value)
-    },
-    [lng, pathname, router],
-  )
-
-  const LANGUAGES = [
-    {
-      key: 'en',
-      label: 'English',
-    },
-    {
-      key: 'zh',
-      label: '简体中文',
-    },
-  ]
-
-  return (
-    <footer className="grid grid-cols-1 justify-items-start px-20 py-[60px] sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-5">
+  return isMobile ? (
+    <FooterMobile lng={lng} />
+  ) : (
+    <footer className="grid grid-cols-1 justify-items-start bg-white px-20 py-[60px] sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-5">
       <div className="flex h-full w-[101px] flex-col gap-6">
         <Image src="/logo.svg" width={48} height={48} alt="muse logo" />
-        <Select defaultValue={lng} value={lng} onValueChange={onLngChange}>
-          <SelectTrigger className="w-full border-none p-0 shadow-none focus:ring-0">
-            <div className="flex items-center gap-1.5">
-              <Icons.locales width={18} height={18} />
-              <p className="text-[14px] font-normal leading-[18.2px] text-[#141414]">
-                <SelectValue placeholder="Language" />
-              </p>
-            </div>
-          </SelectTrigger>
-          <SelectContent className="border-none p-0.5 shadow-[0px_2px_30px_4px_rgba(0,0,0,0.08)]">
-            {languages.map((l, index) => (
-              <SelectItem
-                key={index}
-                value={l}
-                className="cursor-pointer rounded-[8px] font-mono focus:bg-[#F4F5F6] data-[state=checked]:text-[#043FFB]"
-              >
-                {LANGUAGES.find(({ key }) => key === l)?.label}
-              </SelectItem>
-            ))}
-          </SelectContent>
-        </Select>
+        <LngSelector language={lng} />
         <div className="flex flex-1 flex-col justify-end">
           <SocialWidget />
         </div>
