867c0baf54ddbd4b8244e90a0f5265a6b86b78e7
fuxuewei <fuxuewei@tezign.com>
2024-11-22 14:31:24 +0800

    feat: add chinese terms and privacy

--- src/app/[lng]/(company)/terms/page.tsx
+++ src/app/[lng]/(company)/terms/page.tsx
@@ -1,8 +1,8 @@
 /*
  * @Author: fuxuewei fuxuewei@tezign.com
- * @Date: 2024-11-21 15:11:35
+ * @Date: 2024-11-22 10:56:51
  * @LastEditors: fuxuewei fuxuewei@tezign.com
- * @LastEditTime: 2024-11-21 17:03:38
+ * @LastEditTime: 2024-11-22 12:49:34
  * @FilePath: /musedam-website/src/app/[lng]/(company)/terms/page.tsx
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
  */
@@ -10,11 +10,13 @@ import { Metadata } from 'next'
 import TermsPageContent from '@/components/About/policy/TermsContent'
 import { FlexColContainer } from '@/components/StyleWrapper/container'
 import { ssTranslation } from '@/app/i18n'
+import { PropsWithLng } from '@/types/page'
 
-export default function AllFeaturesPage() {
+export default async function AllFeaturesPage({ params }: PropsWithLng) {
+  const { lng } = await params
   return (
     <FlexColContainer className="w-full items-center">
-      <TermsPageContent />
+      <TermsPageContent lng={lng} />
     </FlexColContainer>
   )
 }
--- src/components/About/policy/TermsContent.tsx
+++ src/components/About/policy/TermsContent.tsx
@@ -1,15 +1,19 @@
 'use client'
 
 import { FlexColContainer } from '@/components/StyleWrapper/container'
-import TermsMDX from './terms.mdx'
+import TermsMDXEN from './terms-en.mdx'
+import TermsMDXZH from './terms-zh.mdx'
 import { useEffect, useState } from 'react'
 import useIsMobile from '@/hooks/useIsMobile'
 import './policy.scss'
+import { ssTranslation } from '@/app/i18n'
+import { useTranslation } from '@/app/i18n/client'
 
-export default function TermsPageContent() {
+export default function PrivacyPageContent({ lng }: { lng: string }) {
     const isMobile = useIsMobile()
-
+    const { t } = useTranslation('company')
     const [className, setClassName] = useState('')
+
     useEffect(() => {
         setClassName(isMobile ? 'policy_mobile' : 'policy')
     }, [isMobile])
@@ -18,13 +22,13 @@ export default function TermsPageContent() {
         <FlexColContainer className='w-full items-center'>
             <div className="policy-terms prose mx-auto mt-[30px] w-full max-w-[1140px] px-[20px] pb-20 font-mono md:mt-[80px]">
                 <h1 className="text-center font-baskervville text-[40px] font-normal leading-[46px] md:text-[68px] md:leading-[78px]">
-                    Service Agreement
+                    {t('terms.title')}
                 </h1>
                 <div className="my-[30px] font-mono text-[16px] font-medium leading-[24px] tracking-[0.8px] md:mb-10 md:mt-[60px]">
-                    Version Date: October 31, 2024
+                    {t('terms.update-date')}
                 </div>
                 <div className={className}>
-                    <TermsMDX />
+                    {lng === 'zh' ? <TermsMDXZH /> : <TermsMDXEN />}
                 </div>
             </div>
         </FlexColContainer >
