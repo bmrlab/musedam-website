e5b6cacf4dc1e790951ed949f11c17a86356e58e
fuxuewei <fuxuewei@tezign.com>
2024-11-26 18:38:55 +0800

    fix: privacy and terms in China

--- src/components/About/policy/PrivacyContent.tsx
+++ src/components/About/policy/PrivacyContent.tsx
@@ -25,10 +25,10 @@ export default function PrivacyPageContent({ lng }: { lng: string }) {
         <FlexColContainer className='w-full items-center px-[20px]'>
             <div className="policy-terms prose mx-auto mt-[30px] w-full max-w-[1140px] pb-20 font-mono md:mt-[80px]">
                 <h1 className="text-center font-baskervville text-[40px] font-normal leading-[46px] md:text-[68px] md:leading-[78px]">
-                    {t('privacy.title')}
+                    {isInChina ? '隐私政策' : t('privacy.title')}
                 </h1>
                 <div className="my-[30px] font-mono text-[16px] font-medium leading-[24px] tracking-[0.8px] md:mb-10 md:mt-[60px]">
-                    {t('privacy.update-date')}
+                    {isInChina ? '版本日期：2023年9月18日' : t('privacy.update-date')}
                 </div>
                 <div className={cn('privacy-content', className)}>
                     {isInChina ? <PrivacyInChina /> : lng === 'zh' ? <PrivacyMDXZH /> : <PrivacyMDXEN />}
--- src/components/About/policy/TermsContent.tsx
+++ src/components/About/policy/TermsContent.tsx
@@ -24,13 +24,13 @@ export default function PrivacyPageContent({ lng }: { lng: string }) {
         <FlexColContainer className='w-full items-center'>
             <div className="policy-terms prose mx-auto mt-[30px] w-full max-w-[1140px] px-[20px] pb-20 font-mono md:mt-[80px]">
                 <h1 className="text-center font-baskervville text-[40px] font-normal leading-[46px] md:text-[68px] md:leading-[78px]">
-                    {t('terms.title')}
+                    {isInChina ? '服务协议' : t('terms.title')}
                 </h1>
                 <div className="my-[30px] font-mono text-[16px] font-medium leading-[24px] tracking-[0.8px] md:mb-10 md:mt-[60px]">
-                    {t('terms.update-date')}
+                    {isInChina ? '版本日期：2024年10月18日' : t('terms.update-date')}
                 </div>
                 <div className={className}>
-                    {isInChina ? <TermsInChina /> : lng === 'zh' ? <TermsMDXZH /> : <TermsMDXEN />}
+                    {isInChina ? <TermsInChina /> : (lng === 'zh' ? <TermsMDXZH /> : <TermsMDXEN />)}
                 </div>
             </div>
         </FlexColContainer >
