56406999aade6c727f13ea1c623df68ab110ada1
ZingerLittleBee <6970999@gmail.com>
2024-11-29 15:46:20 +0800

    header bottom border

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -5,7 +5,7 @@ import HeaderMobile from '@/components/Header/mobile'
 
 export default function Header() {
   return (
-    <nav className="fixed z-50 flex h-[56px] w-full items-center border-b border-black bg-white font-mono md:h-[70px]">
+    <nav className="fixed z-50 flex h-[56px] w-full items-center border-b border-[#EBECEE] bg-white font-mono md:h-[70px]">
       <HeaderDesktop className="hidden md:flex" />
       <HeaderMobile className="flex md:hidden" />
     </nav>
