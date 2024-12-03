7ded123ae7d72039fc14b106d7cf3e75731de647
fuxuewei <fuxuewei@tezign.com>
2024-12-03 19:32:05 +0800

    fix: language change

--- src/components/About/OurStory.tsx
+++ src/components/About/OurStory.tsx
@@ -1,29 +1,20 @@
-/*
- * @Author: fuxuewei fuxuewei@tezign.com
- * @Date: 2024-12-03 10:27:16
- * @LastEditors: fuxuewei fuxuewei@tezign.com
- * @LastEditTime: 2024-12-03 18:35:32
- * @FilePath: /musedam-website/src/components/About/OurStory.tsx
- * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
- */
 'use client'
 
 import { twx } from '@/utilities/cn'
 import { FlexColContainer } from '@/components/StyleWrapper/Container'
 import { useTranslation } from '@/app/i18n/client'
-import { useMemo } from 'react'
 
 const Title = twx.p`font-euclid md:text-[80px] text-[54px] font-normal md:leading-[91.52px] leading-[48px] tracking-[1px] text-[#141414]`
 
 export default function HomeHero() {
     const { t } = useTranslation('company')
 
-    const descriptions = useMemo(() => [
+    const descriptions = [
         t("about-us.our-story.p1"),
         t("about-us.our-story.p2"),
         t("about-us.our-story.p3"),
         t("about-us.our-story.p4"),
-    ], [t])
+    ]
 
     return (
 
--- src/components/LandingPage/Highlights/index.tsx
+++ src/components/LandingPage/Highlights/index.tsx
@@ -1,9 +1,7 @@
 'use client'
 
-import { useMemo } from 'react'
-
 import useIsMobile from '@/hooks/useIsMobile'
-import { useHighlightTranslation, useTranslation } from '@/app/i18n/client'
+import { useHighlightTranslation } from '@/app/i18n/client'
 
 import HighlightsDesktop from './desktop'
 import HighlightsMobile from './mobile'
@@ -22,59 +20,56 @@ export type Highlight = {
 export default function Highlights() {
   const { t } = useHighlightTranslation()
   const { getUrl } = usePublicUrl('/Highlights/Mobile')
-  const data = useMemo<Highlight[]>(
-    () => [
-      {
-        key: 'collect',
-        title: t('collect.title'),
-        description: [t('collect.description.0'), t('collect.description.1')],
-        image: getUrl('Collect.png'),
-        bgColor: '#E8E4E4',
-      },
-      {
-        key: 'organize',
-        title: t('organize.title'),
-        description: [t('organize.description.0'), t('organize.description.1')],
-        image: getUrl('Organize.png'),
-        bgColor: '#FFFFFF',
-      },
-      {
-        key: 'collaborate',
-        title: t('collaborate.title'),
-        description: [t('collaborate.description.0')],
-        list: [
-          t('collaborate.list.0'),
-          t('collaborate.list.1'),
-          t('collaborate.list.2'),
-          t('collaborate.list.3'),
-        ],
-        image: getUrl('Collaborate.png'),
-        bgColor: '#D0CCE0',
-      },
-      {
-        key: 'ai-generate',
-        title: t('ai-generate.title'),
-        description: [t('ai-generate.description.0')],
-        point: [
-          {
-            title: t('ai-generate.point.0.title'),
-            description: t('ai-generate.point.0.description'),
-          },
-          {
-            title: t('ai-generate.point.1.title'),
-            description: t('ai-generate.point.1.description'),
-          },
-          {
-            title: t('ai-generate.point.2.title'),
-            description: t('ai-generate.point.2.description'),
-          },
-        ],
-        image: getUrl('AI-Generate.png'),
-        bgColor: '#E8E4E4',
-      },
-    ],
-    [t, getUrl],
-  )
+  const data = [
+    {
+      key: 'collect',
+      title: t('collect.title'),
+      description: [t('collect.description.0'), t('collect.description.1')],
+      image: getUrl('Collect.png'),
+      bgColor: '#E8E4E4',
+    },
+    {
+      key: 'organize',
+      title: t('organize.title'),
+      description: [t('organize.description.0'), t('organize.description.1')],
+      image: getUrl('Organize.png'),
+      bgColor: '#FFFFFF',
+    },
+    {
+      key: 'collaborate',
+      title: t('collaborate.title'),
+      description: [t('collaborate.description.0')],
+      list: [
+        t('collaborate.list.0'),
+        t('collaborate.list.1'),
+        t('collaborate.list.2'),
+        t('collaborate.list.3'),
+      ],
+      image: getUrl('Collaborate.png'),
+      bgColor: '#D0CCE0',
+    },
+    {
+      key: 'ai-generate',
+      title: t('ai-generate.title'),
+      description: [t('ai-generate.description.0')],
+      point: [
+        {
+          title: t('ai-generate.point.0.title'),
+          description: t('ai-generate.point.0.description'),
+        },
+        {
+          title: t('ai-generate.point.1.title'),
+          description: t('ai-generate.point.1.description'),
+        },
+        {
+          title: t('ai-generate.point.2.title'),
+          description: t('ai-generate.point.2.description'),
+        },
+      ],
+      image: getUrl('AI-Generate.png'),
+      bgColor: '#E8E4E4',
+    },
+  ]
 
   const isMobile = useIsMobile()
   return isMobile ? <HighlightsMobile data={data} /> : <HighlightsDesktop data={data} />
