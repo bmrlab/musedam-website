dcb1ce73328174e3e0533854670ec19d6088937b
ZingerLittleBee <6970999@gmail.com>
2024-12-09 10:46:25 +0800

    desktop hero animation

--- src/components/LandingPage/Hero/Banner/Desktop.tsx
+++ src/components/LandingPage/Hero/Banner/Desktop.tsx
@@ -1,3 +1,4 @@
+import Image from 'next/image'
 import { cn } from '@/utilities/cn'
 import { useHover } from '@uidotdev/usehooks'
 import { AnimatePresence } from 'framer-motion'
@@ -6,18 +7,20 @@ import useAnimationTrace from '@/hooks/useAnimationTrace'
 import useIsZhLng from '@/hooks/useIsZhLng'
 import usePublicUrl from '@/hooks/usePublicUrl'
 import {
-  MotionImageWithSkeleton,
-  MotionShadowImageRoundedWithSkeleton,
-  MotionVideoWithSkeleton,
-} from '@/components/ImageWithSkeleton'
-import {
   AbsXCenterContainer,
   FlexColContainer,
   RelativeContainer,
 } from '@/components/StyleWrapper/Container'
-import { MotionImage, MotionShadowImage } from '@/components/StyleWrapper/image'
+import {
+  AnimationImagePreset,
+  BottomToTopImage,
+  LeftToRightImage,
+  MotionImage,
+  RightToLeftImage,
+  TopToBottomImage,
+} from '@/components/StyleWrapper/image'
 
-import { MotionShadowImageRounded, MotionShadowRoundedFullImage } from '.'
+import { MotionShadowImageRounded } from '.'
 
 export default function DesktopBanner({ className }: { className?: string }) {
   const [aiParseTagRef, aiParseTagHovering] = useHover()
@@ -29,19 +32,15 @@ export default function DesktopBanner({ className }: { className?: string }) {
   return (
     <div className={cn('flex select-none justify-center gap-6', className)}>
       <FlexColContainer>
-        <MotionShadowImageRoundedWithSkeleton
+        <TopToBottomImage
           src={getUrl('MuseDAM-Kanban.png')}
           width={237}
           height={52}
-          priority
           alt="MuseDAM-Kanban"
-          initial={{ y: -20, opacity: 0 }}
-          animate={isBuildFinished(18) ? { y: 0, opacity: 1 } : {}}
-          transition={{ duration: 1, ease: 'easeInOut' }}
-          onAnimationComplete={() => handleAnimationComplete(19)}
+          className="rounded-[4.69px] shadow-[0_1.17px_17.6px_2.35px_#00000014] duration-1000 delay-3750"
         />
         <div className="relative">
-          <MotionVideoWithSkeleton
+          <video
             loop
             autoPlay
             muted
@@ -50,129 +49,99 @@ export default function DesktopBanner({ className }: { className?: string }) {
             x5-video-player-fullscreen="false"
             width={237}
             height={315}
-            src="/assets/Hero/MuseDAM-Asset-Car.mp4"
-            type="video/mp4"
-            className="mt-5 h-[315px] rounded-[8px] object-cover"
-            initial={{ x: 20, opacity: 0 }}
-            animate={{ x: 0, opacity: 1 }}
-            transition={{ duration: 0.6, ease: 'easeInOut' }}
-            onAnimationComplete={() => handleAnimationComplete(1)}
-          />
+            className={cn(
+              'mt-5 h-[315px] rounded-[8px] object-cover',
+              'ease-in-out animate-in fade-in slide-in-from-right duration-600 [animation-fill-mode:backwards]',
+            )}
+          >
+            <source src="/assets/Hero/MuseDAM-Asset-Car.mp4" type="video/mp4" />
+          </video>
           <div
             className={cn(
               'absolute bottom-[34px] z-[1] flex flex-col gap-2',
               isZhLng ? 'right-[-32px]' : 'right-[-66px]',
             )}
           >
-            <MotionShadowImage
+            <RightToLeftImage
               src={getUrl('Tag-Automobile.png')}
               width={isZhLng ? 58 : 110}
               height={30}
               priority
               alt="Tag-Automobile"
-              initial={{ x: 20, opacity: 0 }}
-              animate={isBuildFinished(14) ? { x: 0, opacity: 1 } : {}}
-              transition={{ duration: 0.6, delay: 0.1, ease: 'easeInOut' }}
-              onAnimationComplete={() => handleAnimationComplete(16)}
+              className="shadow-[0px_4px_30px_4px_#00000014] duration-600 delay-2650"
             />
-            <MotionShadowRoundedFullImage
+            <RightToLeftImage
               src={getUrl('Tag-Raining.png')}
               width={isZhLng ? 71 : 87}
               height={30}
               priority
               alt="Tag-Raining"
-              initial={{ x: 20, opacity: 0 }}
-              animate={isBuildFinished(14) ? { x: 0, opacity: 1 } : {}}
-              transition={{ duration: 0.8, delay: 0.2, ease: 'easeInOut' }}
-              onAnimationComplete={() => handleAnimationComplete(17)}
+              className="shadow-[0px_4px_30px_4px_#00000014] duration-800 delay-2750"
             />
-            <MotionShadowRoundedFullImage
+            <RightToLeftImage
               src={getUrl('Tag-Photography.png')}
               width={isZhLng ? 84 : 118}
               height={30}
               priority
               alt="Tag-Photography"
-              initial={{ x: 20, opacity: 0 }}
-              animate={isBuildFinished(14) ? { x: 0, opacity: 1 } : {}}
-              transition={{ duration: 1, delay: 0.2, ease: 'easeInOut' }}
-              onAnimationComplete={() => handleAnimationComplete(18)}
+              className="shadow-[0px_4px_30px_4px_#00000014] duration-1000 delay-2750"
             />
           </div>
         </div>
         <div className="mt-4 flex gap-[11px]">
-          <MotionShadowImageRoundedWithSkeleton
+          <RightToLeftImage
             src="/assets/Hero/MuseDAM-Asset-Car-V2.png"
             width={80}
             height={80}
             priority
             alt="MuseDAM-Asset-Car-V2"
-            initial={{ x: 20, opacity: 0 }}
-            animate={isBuildFinished(1) ? { opacity: 1, x: 0 } : {}}
-            transition={{ duration: 0.8, ease: 'easeInOut' }}
-            onAnimationComplete={() => handleAnimationComplete(2)}
           />
-          <MotionShadowImageRoundedWithSkeleton
+          <RightToLeftImage
             src="/assets/Hero/MuseDAM-Asset-Car-V3.png"
             width={115}
             height={79}
             priority
             alt="MuseDAM-Asset-Car-V3"
-            initial={{ x: 20, opacity: 0 }}
-            animate={isBuildFinished(1) ? { opacity: 1, x: 0 } : {}}
-            transition={{ duration: 1, ease: 'easeInOut' }}
-            onAnimationComplete={() => handleAnimationComplete(3)}
           />
         </div>
       </FlexColContainer>
       <FlexColContainer className="mt-[50px] gap-[25px]">
-        <MotionShadowImageRoundedWithSkeleton
+        <RightToLeftImage
           src="/assets/Hero/MuseDAM-Asset-Poster.png"
           width={237}
           height={275}
           priority
           alt="MuseDAM-Asset-Poster"
-          initial={{ x: 20, opacity: 0 }}
-          animate={isBuildFinished(1) ? { opacity: 1, x: 0 } : {}}
-          transition={{ duration: 0.6, delay: 0.05, ease: 'easeInOut' }}
-          onAnimationComplete={() => handleAnimationComplete(4)}
+          className="duration-600 delay-50"
         />
         <FlexColContainer className="gap-[11px]">
-          <MotionShadowImageRoundedWithSkeleton
+          <RightToLeftImage
             src="/assets/Hero/MuseDAM-Asset-Jewellery.png"
             width={237}
             height={175}
             priority
             alt="MuseDAM-Asset-Jewellery"
-            initial={{ x: 20, opacity: 0 }}
-            animate={isBuildFinished(1) ? { opacity: 1, x: 0 } : {}}
-            transition={{ duration: 0.6, delay: 0.05, ease: 'easeInOut' }}
-            onAnimationComplete={() => handleAnimationComplete(5)}
+            className="duration-600 delay-50"
           />
-          <MotionImageWithSkeleton
+          <BottomToTopImage
             src="/assets/Hero/MuseDAM-AI-Color.png"
             width={237}
             height={38}
             priority
             alt="MuseDAM-AI-Color"
-            initial={{ y: 20, opacity: 0 }}
-            animate={isBuildFinished(18) ? { opacity: 1, y: 0 } : {}}
-            transition={{ duration: 1, ease: 'easeInOut' }}
-            onAnimationComplete={() => handleAnimationComplete(20)}
+            className="duration-1000 delay-3750"
           />
         </FlexColContainer>
       </FlexColContainer>
       <FlexColContainer className="gap-6">
         <RelativeContainer>
-          <MotionShadowImageRoundedWithSkeleton
+          <RightToLeftImage
             src="/assets/Hero/MuseDAM-Asset-3D.png"
             width={237}
             height={295}
             priority
             alt="MuseDAM-Asset-3D"
-            initial={{ x: 20, opacity: 0 }}
-            animate={isBuildFinished(1) ? { opacity: 1, x: 0 } : {}}
-            transition={{ duration: 0.6, delay: 0.05, ease: 'easeInOut' }}
-            onAnimationComplete={() => handleAnimationComplete(6)}
+            className="duration-600 delay-50"
           />
           <MotionImage
             src={getUrl('MuseDAM-Comment.png')}
@@ -188,30 +157,23 @@ export default function DesktopBanner({ className }: { className?: string }) {
           />
         </RelativeContainer>
         <RelativeContainer>
-          <MotionShadowImageRoundedWithSkeleton
+          <RightToLeftImage
             src="/assets/Hero/MuseDAM-Asset-Photography.png"
             width={237}
             height={285}
             priority
             alt="MuseDAM-Asset-Photography"
-            initial={{ x: 20, opacity: 0 }}
-            animate={isBuildFinished(1) ? { opacity: 1, x: 0 } : {}}
-            transition={{ duration: 0.6, delay: 0.05, ease: 'easeInOut' }}
-            onAnimationComplete={() => handleAnimationComplete(7)}
+            className="duration-600 delay-50"
           />
           <RelativeContainer>
-            <MotionImage
+            <AnimationImagePreset
               ref={aiParseTagRef}
               src="/assets/Hero/MuseDAM-AI-Parsing-Tag.png"
               width={24}
               height={20}
               priority
               alt="MuseDAM-AI-Parsing-Tag"
-              className="absolute bottom-[8px] right-[9px] z-[1] cursor-pointer"
-              initial={{ opacity: 0 }}
-              animate={isBuildFinished(22) ? { opacity: 1 } : {}}
-              transition={{ duration: 0.6, ease: 'easeInOut' }}
-              onAnimationComplete={() => handleAnimationComplete(24)}
+              className="absolute bottom-[8px] right-[9px] z-[1] cursor-pointer duration-600 delay-5350"
             />
             <div className="absolute bottom-[30px] right-[3px] h-[290px] w-[272px] object-cover">
               <AnimatePresence mode="wait">
@@ -232,160 +194,120 @@ export default function DesktopBanner({ className }: { className?: string }) {
         </RelativeContainer>
       </FlexColContainer>
       <FlexColContainer className="mt-[50px] gap-[24px]">
-        <MotionShadowImageRoundedWithSkeleton
+        <LeftToRightImage
           src="/assets/Hero/MuseDAM-Asset-Product.png"
           width={237}
           height={235}
           priority
           alt="MuseDAM-Asset-Product"
-          initial={{ x: -20, opacity: 0 }}
-          animate={isBuildFinished(1) ? { opacity: 1, x: 0 } : {}}
-          transition={{ duration: 0.6, delay: 0.05, ease: 'easeInOut' }}
-          onAnimationComplete={() => handleAnimationComplete(8)}
+          className="duration-600 delay-50"
         />
         <RelativeContainer className="flex justify-center">
-          <MotionShadowImageRoundedWithSkeleton
+          <LeftToRightImage
             src="/assets/Hero/MuseDAM-Asset-Group3.png"
             width={196.8}
             height={275}
             priority
             alt="MuseDAM-Asset-Group3"
-            containerClassName="absolute"
-            initial={{ x: -20, opacity: 0 }}
-            animate={isBuildFinished(1) ? { opacity: 1, x: 0 } : {}}
-            transition={{ duration: 0.6, ease: 'easeInOut' }}
-            onAnimationComplete={() => handleAnimationComplete(9)}
+            className="absolute duration-600"
           />
-          <MotionShadowImageRoundedWithSkeleton
+          <BottomToTopImage
             src="/assets/Hero/MuseDAM-Asset-Group2.png"
             width={216.8}
             height={285}
             priority
             alt="MuseDAM-Asset-Group2"
-            containerClassName="absolute top-[9px]"
-            initial={{ y: 20, opacity: 0 }}
-            animate={isBuildFinished(11) ? { opacity: 1, y: 0 } : {}}
-            transition={{ duration: 0.5, delay: 0.1, ease: 'easeInOut' }}
-            onAnimationComplete={() => handleAnimationComplete(12)}
+            className="absolute top-[9px] duration-500 delay-750"
           />
-          <MotionShadowImageRoundedWithSkeleton
+          <BottomToTopImage
             src="/assets/Hero/MuseDAM-Asset-Group1.png"
             width={236.8}
             height={285}
             alt="MuseDAM-Asset-Group1"
-            containerClassName="absolute top-[19px]"
-            initial={{ y: 20, opacity: 0 }}
-            animate={isBuildFinished(12) ? { opacity: 1, y: 0 } : {}}
-            transition={{ duration: 0.6, delay: 0.2, ease: 'easeInOut' }}
-            onAnimationComplete={() => handleAnimationComplete(13)}
+            className="absolute top-[19px] duration-600 delay-1450"
           />
           <div className="absolute right-2 top-[27px] flex gap-1">
-            <MotionImage
+            <AnimationImagePreset
               src="/assets/Hero/MuseDAM-Asset-Group-Tag.png"
               width={35}
               height={20}
               priority
               alt="MuseDAM-Asset-Group-Tag"
-              initial={{ opacity: 0 }}
-              animate={isBuildFinished(13) ? { opacity: 1 } : {}}
-              transition={{ duration: 0.5, delay: 0, ease: 'easeInOut' }}
-              onAnimationComplete={() => handleAnimationComplete(14)}
+              className="duration-500 delay-2050"
             />
-            <MotionImage
+            <AnimationImagePreset
               src="/assets/Hero/MuseDAM-Asset-Comment-Tag.png"
               width={24}
               height={20}
               priority
               alt="MuseDAM-Asset-Comment-Tag"
-              initial={{ opacity: 0 }}
-              animate={isBuildFinished(14) ? { opacity: 1 } : {}}
-              transition={{ duration: 0.5, delay: 0.2, ease: 'easeInOut' }}
-              onAnimationComplete={() => handleAnimationComplete(15)}
+              className="duration-500 fade-in delay-2750"
             />
           </div>
         </RelativeContainer>
       </FlexColContainer>
       <FlexColContainer className="gap-6">
         <RelativeContainer>
-          <MotionShadowImageRoundedWithSkeleton
+          <LeftToRightImage
             src="/assets/Hero/MuseDAM-Asset-Cat.png"
             width={237}
             height={275}
             priority
             alt="MuseDAM-Asset-Cat"
-            initial={{ x: -20, opacity: 0 }}
-            animate={isBuildFinished(1) ? { opacity: 1, x: 0 } : {}}
-            transition={{ duration: 0.6, delay: 0.05, ease: 'easeInOut' }}
-            onAnimationComplete={() => handleAnimationComplete(10)}
+            className="duration-600 delay-50"
           />
-          <MotionImage
+          <AnimationImagePreset
             ref={assetMoreRef}
             src="/assets/Hero/MuseDAM-Asset-More.png"
             width={30}
             height={30}
             priority
             alt="MuseDAM-Asset-More.png"
-            className="absolute right-2 top-2.5 cursor-pointer"
-            initial={{ opacity: 0 }}
-            animate={isBuildFinished(22) ? { opacity: 1 } : {}}
-            transition={{ duration: 1, ease: 'easeInOut' }}
-            onAnimationComplete={() => handleAnimationComplete(25)}
-          ></MotionImage>
+            className="absolute right-2 top-2.5 cursor-pointer delay-5350"
+          ></AnimationImagePreset>
           <AnimatePresence mode="wait">
             {assetMoreHovering && (
-              <MotionImage
+              <AnimationImagePreset
                 src="/assets/Hero/MuseDAM-Asset-Dropdown.png"
                 width={171}
                 height={194}
                 alt="MuseDAM-Asset-Dropdown.png"
-                className="absolute right-2 top-[42px]"
-                initial={{ opacity: 0 }}
-                animate={{ opacity: 1 }}
-                exit={{ opacity: 0 }}
-                transition={{ duration: 0.8, delay: 0, ease: 'easeInOut' }}
-              ></MotionImage>
+                className="absolute right-2 top-[42px] duration-800"
+              ></AnimationImagePreset>
             )}
           </AnimatePresence>
         </RelativeContainer>
         <FlexColContainer className="relative">
-          <MotionVideoWithSkeleton
+          <video
             loop
             autoPlay
             muted
             playsInline
             width={237}
             height={315}
-            src="/assets/Hero/MuseDAM-Asset-Video.mp4"
-            type="video/mp4"
-            className="h-[315px] rounded-[8px] object-cover"
-            initial={{ x: -20, opacity: 0 }}
-            animate={isBuildFinished(1) ? { opacity: 1, x: 0 } : {}}
-            transition={{ duration: 0.6, delay: 0.05, ease: 'easeInOut' }}
-            onAnimationComplete={() => handleAnimationComplete(11)}
-          />
-          <MotionShadowImageRounded
+            className={cn(
+              'h-[315px] rounded-[8px] object-cover',
+              'animate-in fade-in slide-in-from-left duration-600 delay-50 [animation-fill-mode:backwards]',
+            )}
+          >
+            <source src="/assets/Hero/MuseDAM-Asset-Video.mp4" type="video/mp4" />
+          </video>
+          <BottomToTopImage
             src="/assets/Hero/MuseDAM-Asset-VideoPlayer.png"
             width={237}
             height={40}
             priority
             alt="MuseDAM-Asset-VideoPlayer"
-            className="mt-[9px]"
-            initial={{ y: '10%', opacity: 0 }}
-            animate={isBuildFinished(18) ? { opacity: 1, y: 0 } : {}}
-            transition={{ duration: 1, ease: 'easeInOut' }}
-            onAnimationComplete={() => handleAnimationComplete(21)}
+            className="mt-[9px] duration-1000 delay-3750"
           />
           <AbsXCenterContainer className="bottom-[7px]">
-            <MotionImage
+            <BottomToTopImage
               src="/assets/Hero/MuseDAM-Video-Frame.png"
               width={57.22}
               height={139}
               priority
               alt="MuseDAM-Video-Frame.png"
-              initial={{ y: '5%', opacity: 0 }}
-              animate={isBuildFinished(21) ? { opacity: 1, y: 0 } : {}}
-              transition={{ duration: 0.6, ease: 'easeInOut' }}
-              onAnimationComplete={() => handleAnimationComplete(22)}
+              className="duration-600 delay-4750"
             />
           </AbsXCenterContainer>
         </FlexColContainer>
