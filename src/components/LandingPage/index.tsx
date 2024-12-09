8f6a08f276ee45bdb7a3f2b4c12787cdeba91030
ZingerLittleBee <6970999@gmail.com>
2024-12-09 18:14:02 +0800

    remove loading page

--- src/components/LandingPage/index.tsx
+++ src/components/LandingPage/index.tsx
@@ -1,26 +1,22 @@
-import { Suspense } from 'react'
 import { twx } from '@/utilities/cn'
 
 import { PropsWithLng } from '@/types/page'
 import Hero from '@/components/LandingPage/Hero'
 import Highlights from '@/components/LandingPage/Highlights'
-import Loading from '@/components/LandingPage/loading'
 import Precept from '@/components/LandingPage/Precept'
 import SubscribeBlock from '@/components/LandingPage/Subscribe'
 
 export default async function LandingPage({ params }: PropsWithLng) {
   const { lng } = await params
   return (
-    <Suspense fallback={<Loading />}>
-      <div className="grid w-screen justify-items-center bg-white">
-        <ContainerWithMaxWidth>
-          <Hero lng={lng} />
-        </ContainerWithMaxWidth>
-        <SubscribeBlock lng={lng} className="mt-[60px] w-full md:mt-[80px]" />
-        <Highlights />
-        <Precept />
-      </div>
-    </Suspense>
+    <div className="grid w-screen justify-items-center bg-white">
+      <ContainerWithMaxWidth>
+        <Hero lng={lng} />
+      </ContainerWithMaxWidth>
+      <SubscribeBlock lng={lng} className="mt-[60px] w-full md:mt-[80px]" />
+      <Highlights />
+      <Precept />
+    </div>
   )
 }
 
