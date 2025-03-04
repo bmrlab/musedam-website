bc1ad48e5924300ae2add83b3d1df824a8b45390
zhuojg <zhuojg1519@outlook.com>
2025-03-04 23:29:19 +0800

    update mainland settings url

--- src/constant/url.ts
+++ src/constant/url.ts
@@ -3,6 +3,6 @@ export const BROWSER_EXTENSION_URL =
   'https://chrome.google.com/webstore/detail/musedam-extension/pgfabgncjjnknojglnhjlcfjhgjlocog'
 export const BROWSER_EXTENSION_URL_ZH = 'https://tezign.feishu.cn/wiki/wikcnAlxBEhe4KNaEpijwk8lhAh'
 export const MUSE_GLOBAL_CHECKOUT_URL = '/settings'
-export const MUSE_MAINLAND_CHECKOUT_URL = 'https://app.museai.cc'
+export const MUSE_MAINLAND_CHECKOUT_URL = 'https://app.museai.cc/settings'
 export const MUSE_GLOBAL_SERVER_URL = "/musedam-apigw"
 export const MUSE_MAINLAND_SERVER_URL = "https://musedam-service.tezign.com"
