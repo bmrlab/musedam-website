831de844e598eea7fd7f88f33c9c13d46a1dc111
ZingerLittleBee <6970999@gmail.com>
2025-07-22 17:56:19 +0800

    feat(Posts): Add support for H5 headings
    Adds support for H5 headings in the Posts collection. This
    allows authors to use a wider range of heading sizes when
    creating content, providing more flexibility in their
    page structure.

--- src/collections/Posts/index.ts
+++ src/collections/Posts/index.ts
@@ -74,7 +74,7 @@ export const Posts: CollectionConfig = {
                 features: ({ rootFeatures }) => {
                   return [
                     ...rootFeatures,
-                    HeadingFeature({ enabledHeadingSizes: ['h1', 'h2', 'h3', 'h4'] }),
+                    HeadingFeature({ enabledHeadingSizes: ['h1', 'h2', 'h3', 'h4', 'h5'] }),
                     BlocksFeature({ blocks: [Banner, Code, MediaBlock] }),
                     FixedToolbarFeature(),
                     InlineToolbarFeature(),
