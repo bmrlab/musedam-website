d3bb6116cf80998975da7ee0f8e55c69f4dad730
ZingerLittleBee <6970999@gmail.com>
2025-07-28 15:48:26 +0800

    feat(TopArticles): Improve layout and styling
    Refactor the `TopArticles` component to enhance the layout and styling:
    
    - Remove unnecessary class name from the section element
    - Improve the responsive layout by adjusting the grid and overflow styles
    - Ensure the content is centered on smaller screens by using the `mx-[calc(50.6%-50vw)]` class
    - Increase the left and right padding on larger screens to maintain consistent spacing
    
    These changes aim to provide a better visual experience for the top articles section.

--- src/components/Blog/TopArticles.tsx
+++ src/components/Blog/TopArticles.tsx
@@ -11,7 +11,7 @@ interface TopArticlesProps {
 
 export const TopArticles: React.FC<TopArticlesProps> = ({ articles, className }) => {
   return (
-    <section className={cn('', className)}>
+    <section className={cn(className)}>
       {/* 标题区域 */}
       <div className="px-6 md:px-[80px]">
         <h2 className="text-center !font-feature text-[38px]/[38px] font-medium text-black md:text-start md:text-[54px]/[54px]">
@@ -20,7 +20,12 @@ export const TopArticles: React.FC<TopArticlesProps> = ({ articles, className })
       </div>
 
       {/* 文章列表 */}
-      <div className="md:no-scrollbar flex flex-col  gap-[30px] px-6 pt-[30px] md:grid md:w-full md:auto-cols-max md:grid-flow-col md:gap-[30px] md:overflow-x-auto md:px-[80px] md:pt-[60px]">
+      <div
+        className={cn(
+          ' flex flex-col gap-[30px] px-6 pt-[30px]  md:gap-[30px]',
+          'md:no-scrollbar md:mx-[calc(50.6%-50vw)] md:grid md:auto-cols-max md:grid-flow-col md:overflow-x-auto md:px-[max(80px,calc(50vw-50%+80px))] md:pt-[60px] ',
+        )}
+      >
         {articles.map((article) => (
           <ArticleCard
             key={article.id}
