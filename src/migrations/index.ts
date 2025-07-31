1c6ae7a733677a79c48d9d5e46bc79f4e245465b
ZingerLittleBee <6970999@gmail.com>
2025-07-31 19:21:34 +0800

    feat(i18n): Add new translations for pages and blocks
    The changes in this commit add a new migration file for i18n translations. The
    key changes include:
    
    - Add a new table `pages_hero_links` to store hero link data for pages
    - Add a new table `pages_blocks_cta_links` to store CTA link data for page
      blocks
    - Add new enum types for link types and appearances
    - Add foreign key constraints and indexes to improve data integrity and
      performance
    
    These changes are necessary to support the internationalization of the pages and
    blocks features in the application.

--- src/migrations/index.ts
+++ src/migrations/index.ts
@@ -1,5 +1,6 @@
 import * as migration_20241202_093459 from './20241202_093459';
 import * as migration_20250728_032643_article_properties from './20250728_032643_article_properties';
+import * as migration_20250731_112044_i18n from './20250731_112044_i18n';
 
 export const migrations = [
   {
@@ -10,6 +11,11 @@ export const migrations = [
   {
     up: migration_20250728_032643_article_properties.up,
     down: migration_20250728_032643_article_properties.down,
-    name: '20250728_032643_article_properties'
+    name: '20250728_032643_article_properties',
+  },
+  {
+    up: migration_20250731_112044_i18n.up,
+    down: migration_20250731_112044_i18n.down,
+    name: '20250731_112044_i18n'
   },
 ];
