da0d83a6ab0c60cab934b0779c1af713dccb653a
ZingerLittleBee <6970999@gmail.com>
2025-08-01 15:27:08 +0800

    feat(migrations): add image prefix column to media table
    The changes in this commit add a new column `prefix` to the `media` table in the database. This column will store a default prefix for image URLs, which can be used to construct the full URL for an image.
    
    The `up` function in the migration script adds the new column with a default value of `'musedam-website/media'`, while the `down` function removes the column if it exists.
    
    This change is necessary to ensure that all image URLs in the application have a consistent prefix, which can be useful for managing and organizing media assets.

--- src/migrations/index.ts
+++ src/migrations/index.ts
@@ -1,6 +1,7 @@
 import * as migration_20241202_093459 from './20241202_093459';
 import * as migration_20250728_032643_article_properties from './20250728_032643_article_properties';
 import * as migration_20250731_112044_i18n from './20250731_112044_i18n';
+import * as migration_20250801_072637_image_prefix from './20250801_072637_image_prefix';
 
 export const migrations = [
   {
@@ -16,6 +17,11 @@ export const migrations = [
   {
     up: migration_20250731_112044_i18n.up,
     down: migration_20250731_112044_i18n.down,
-    name: '20250731_112044_i18n'
+    name: '20250731_112044_i18n',
+  },
+  {
+    up: migration_20250801_072637_image_prefix.up,
+    down: migration_20250801_072637_image_prefix.down,
+    name: '20250801_072637_image_prefix'
   },
 ];
