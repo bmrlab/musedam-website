2c971a397acb51510a6ad1f6cd0b2750225c57b2
ZingerLittleBee <6970999@gmail.com>
2025-08-06 15:47:22 +0800

    feat: Remove unused BeforeDashboard component
    The changes in this commit remove the unused `BeforeDashboard` component from the application. This component was previously used to render the 'welcome' block after logging into the admin panel, but it is no longer needed. The corresponding import statement and usage in the `importMap.js` file have also been removed.
    
    fix: Update blog page path
    
    This commit updates the blog page path in the `sitemap.ts` file from `/blog/posts` to `/blog`. This change ensures that the blog pages are correctly generated and accessible.
    
    refactor: Update posts collection prefix
    
    The changes in this commit update the `collectionPrefixMap` in the `generatePreviewPath.ts` file to use the `/blog` prefix for the `posts` collection. This change aligns the preview path generation with the updated blog page path.

--- src/app/(payload)/admin/importMap.js
+++ src/app/(payload)/admin/importMap.js
@@ -22,7 +22,6 @@ import { HorizontalRuleFeatureClient as HorizontalRuleFeatureClient_e70f5e05f09f
 import { BlocksFeatureClient as BlocksFeatureClient_e70f5e05f09f93e00b997edb1ef0c864 } from '@payloadcms/richtext-lexical/client'
 import { LinkToDoc as LinkToDoc_aead06e4cbf6b2620c5c51c9ab283634 } from '@payloadcms/plugin-search/client'
 import { ReindexButton as ReindexButton_aead06e4cbf6b2620c5c51c9ab283634 } from '@payloadcms/plugin-search/client'
-import { default as default_1a7510af427896d367a49dbf838d2de6 } from '@/components/BeforeDashboard'
 import { default as default_8a7ab0eb7ab5c511aba12e68480bfe5e } from '@/components/BeforeLogin'
 import { S3ClientUploadHandler as S3ClientUploadHandler_f97aa6c64367fa259c5bc0567239ef24 } from '@payloadcms/storage-s3/client'
 
@@ -51,7 +50,6 @@ export const importMap = {
   "@payloadcms/richtext-lexical/client#BlocksFeatureClient": BlocksFeatureClient_e70f5e05f09f93e00b997edb1ef0c864,
   "@payloadcms/plugin-search/client#LinkToDoc": LinkToDoc_aead06e4cbf6b2620c5c51c9ab283634,
   "@payloadcms/plugin-search/client#ReindexButton": ReindexButton_aead06e4cbf6b2620c5c51c9ab283634,
-  "@/components/BeforeDashboard#default": default_1a7510af427896d367a49dbf838d2de6,
   "@/components/BeforeLogin#default": default_8a7ab0eb7ab5c511aba12e68480bfe5e,
   "@payloadcms/storage-s3/client#S3ClientUploadHandler": S3ClientUploadHandler_f97aa6c64367fa259c5bc0567239ef24
 }
